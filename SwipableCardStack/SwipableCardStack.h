bca16fa7db6c945daad9503108f4f32a6c682363
yunuki <woogie_ing@naver.com>
2021-12-08 02:57:00 +0900

    Initial Commit

---
+++ SwipableCardStack/SwipableCardStack.h
@@ -0,0 +1,18 @@
+//
+//  SwipableCardStack.h
+//  SwipableCardStack
+//
+//  Created by 윤재욱 on 2021/12/08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwipableCardStack.
+FOUNDATION_EXPORT double SwipableCardStackVersionNumber;
+
+//! Project version string for SwipableCardStack.
+FOUNDATION_EXPORT const unsigned char SwipableCardStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipableCardStack/PublicHeader.h>
+
+
